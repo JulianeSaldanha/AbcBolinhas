b794c2ce4951d326b29c07d3e68c09be71366f6c
JulianeSaldanha <ju.saldanha@hotmail.com>
2016-10-14 20:01:14 -0300

    Teste

---
+++ src/imposto/Orcamento.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imposto;
+
+/**
+ *
+ * @author Acer
+ */
+public class Orcamento {
+    private double valor;
+public Orcamento (double valor){
+this.valor = valor;
+}
+public double getValor(){
+return valor;
+}
+}
